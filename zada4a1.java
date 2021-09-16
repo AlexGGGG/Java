982bdd73e6f783980fdf5ae9c423151bad96d09f
Aleksandr Gratsjov <73903996+AlexGGGG@users.noreply.github.com>
2021-09-16 18:00:14 +0300

    задание 1
    ver 1.0

---
+++ zada4a1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication196;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class JavaApplication196 {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+        int x = new Scanner(System.in).nextInt();
+        int summ = 0;
+        System.out.println("Единиц: " + x % 10);
+        System.out.println("Десятков: " + x / 10 % 10);
+        System.out.println("Сотен: " + x / 100);
+        while (x > 0) {
+            summ += x % 10;
+            x /= 10;
+        }
+        System.out.println("Сумма цифр: " + summ);
+}
